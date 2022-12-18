9aa8a168ae348628c00d9421196f84da03c5c43e
Taewan Kim <terry960302@gmail.com>
2022-12-18 18:27:14 +0900

    :sparkles: dev환경에서 팀원만 sms 가도록 제한

--- handlers/sms-handler.go
+++ handlers/sms-handler.go
@@ -2,12 +2,14 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"marketbill-messaging-service/constants"
 	"marketbill-messaging-service/datastore"
 	"marketbill-messaging-service/models"
 	"marketbill-messaging-service/services"
 	"net/http"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 )
@@ -25,6 +27,11 @@ func HandleSMS(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRes
 	if err := json.Unmarshal([]byte(request.Body), &req); err != nil {
 		e := fmt.Sprintf("[HandleSMS] Unmarshal > %s", err.Error())
 		return r.Error(http.StatusInternalServerError, e)
+	}
+
+	// dev 환경에서는 팀 내부에서만 SMS 보낼 수 있도록 제한
+	if err := validatePhoneNoInDev(req.To); err != nil {
+		r.Error(http.StatusBadRequest, err.Error())
 	}
 
 	switch req.MessageType {
@@ -68,4 +75,26 @@ func HandleSMS(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRes
 	default:
 		return r.Error(http.StatusBadRequest, "Bad Request : Wrong message type")
 	}
+}
+
+func validatePhoneNoInDev(phoneNo string) error {
+	profile := os.Getenv("PROFILE")
+	if profile == "dev" {
+		VALID_PHONE_NUMBERS := []string{"01091751159", "01099457328", "01096782724"} // 김태완, 안중석, 김소진(?)
+
+		count := 0
+		for _, p := range VALID_PHONE_NUMBERS {
+			if p == phoneNo {
+				count += 1
+			}
+		}
+
+		if count <= 0 {
+			err := errors.New("invalid phone no to use in 'DEV' environment")
+			return err
+		} else {
+			return nil
+		}
+	}
+	return nil
 }
